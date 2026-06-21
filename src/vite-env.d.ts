a3004cdef10f2720f7f1e84edddb95113e38fd6b
GCE <PeterWilson@GCEstack.onmicrosoft.com>
2026-06-21 14:21:42 -0500

    chore(mobile): add OpenAI/Deepgram env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_SENTRY_DSN?: string;
   readonly VITE_APP_VERSION?: string;
   readonly VITE_VERCEL_ENV?: string;
+  readonly VITE_OPENAI_API_KEY?: string;
+  readonly VITE_DEEPGRAM_API_KEY?: string;
 }
 
 interface ImportMeta {
